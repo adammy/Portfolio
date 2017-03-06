cf83c34e70bfdb492a397ba5855b33ec18fd5454
adammy <adammacdonaldcorrao@gmail.com>
2017-03-06 12:19:21 -0500

    Edits

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,46 +3,62 @@
   'use strict';
 
   var skills = [
-    { name: "HTML", number: 100 },
-    { name: "CSS", number: 100 },
-    { name: "LESS", number: 100 },
-    { name: "SASS", number: 100 },
-    { name: "SCSS", number: 100 },
-    { name: "JavaScript", number: 80 },
-    { name: "jQuery", number: 80 },
-    { name: "Angular", number: 60 },
-    { name: "Node", number: 60 },
-    { name: "PHP", number: 60 },
-    { name: "PhotoShop", number: 80 },
-    { name: "Illustrator", number: 50 },
-    { name: "Grunt", number: 70 },
-    { name: "Gulp", number: 70 },
-    { name: "Git", number: 70 }
+    { name: 'HTML', number: 100 },
+    { name: 'CSS', number: 100 },
+    { name: 'LESS', number: 100 },
+    { name: 'SASS & SCSS', number: 100 },
+    { name: 'JavaScript', number: 80 },
+    { name: 'jQuery', number: 80 },
+    { name: 'Angular', number: 60 },
+    { name: 'Node', number: 60 },
+    { name: 'PHP', number: 60 },
+    { name: 'SQL', number: 60 },
+    { name: 'PhotoShop', number: 80 },
+    { name: 'Illustrator', number: 50 },
+    { name: 'Grunt', number: 70 },
+    { name: 'Gulp', number: 70 },
+    { name: 'Git', number: 70 }
   ];
 
-  d3.select(".chart")
-    .selectAll("div")
+
+
+  d3.select('#chart')
+    .selectAll('div')
     .data(skills)
     .enter()
-    .append("div")
-    .classed("skill-circle", true)
-    .style("width", function (d) {
-      return (d.number * 1.2) + "px";
+    .append('div')
+    .classed('skill', true)
+    .style('width', function (d) {
+      return (d.number * 1) + '%';
     })
-    .style("height", function (d) {
-      return (d.number * 1.2) + "px";
+    .style('height', function (d) {
+      return (d.number * 1) + '%';
     })
     .text(function (d) {
       return d.name;
     });
 
-  /*
-  $('.work-item').waypoint({
-    handler: function () {
-      $("#" + this.element.id).find(".typer").addClass("typing");
-    },
-    offset: 200
+  $(window).on('scroll', function () {
+    var scrollPos = $(window).scrollTop();
+    if (scrollPos <= 0) {
+      $("nav").removeClass("active");
+    } else {
+      $("nav").addClass("active");
+    }
   });
-  */
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 40
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 
 }());
