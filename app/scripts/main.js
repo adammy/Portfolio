4db1c7b8d7133e5eb9ed71e43529df711ccc380b
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2017-05-31 16:58:51 -0400

    Utilizing video in device sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,5 +2,17 @@
 
   'use strict';
 
+	$('#work-slider').slick({
+		arrows: false,
+		dots: false
+	});
+
+	$('video').on('mouseenter', function () {
+		$(this).get(0).play();
+	}).on('mouseleave', function () {
+		$(this).get(0).pause();
+		//$(this).get(0).currentTime = 0;
+		//$(this).get(0).load();
+	});
 
 }());
