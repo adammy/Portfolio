127b8c80e00bed8fa54bc9de775d8fc6aad467c7
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2017-06-01 15:23:23 -0400

    Copy for works; animation for address bar; additional styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,39 @@
 
   'use strict';
 
-	$('#work-slider').slick({
+	const WORKSLIDER = $('#work-slider');
+
+	WORKSLIDER.slick({
 		arrows: false,
 		dots: false
+	});
+
+	WORKSLIDER.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+
+		// get slide elements
+		let prevSlide = $(slick.$slides.get(currentSlide));
+
+		// previous slide videos reset time to beginning (rewind)
+		prevSlide.find('video').get(0).currentTime = 0;
+		prevSlide.find('video').get(1).currentTime = 0;
+
+		// reload previous slide videos with new start time
+		setTimeout(function () {
+			prevSlide.find('video').get(0).load();
+			prevSlide.find('video').get(1).load();
+		}, 500);
+
+	});
+
+	WORKSLIDER.on('afterChange', function (event, slick, currentSlide) {
+
+		// get slide elements
+		let slide = $(slick.$slides.get(currentSlide));
+
+		// next slide types address bar
+		$('.slide').find('.typer').removeClass('typing');
+		slide.find('.typer').addClass('typing');
+
 	});
 
 	$('video').on('mouseenter', function () {
